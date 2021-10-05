fe62858863884c465efbf537d5a78b1a248949cf
Milan Tarami <milantarami.dev@gmail.com>
2021-10-06 01:20:55 +0545

    first commit

---
+++ lib/widgets/conversation_list.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+import '../screens/chat_detail_page.dart';
+
+class ConversationList extends StatefulWidget {
+  final String name;
+  final String messageText;
+  final String imageUrl;
+  final String time;
+  final bool isMessageRead;
+
+  const ConversationList({
+    Key? key,
+    required this.name,
+    required this.messageText,
+    required this.imageUrl,
+    required this.time,
+    required this.isMessageRead,
+  }) : super(key: key);
+
+  @override
+  _ConversationListState createState() => _ConversationListState();
+}
+
+class _ConversationListState extends State<ConversationList> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const ChatDetailPage();
+          }));
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(widget.imageUrl),
+                      maxRadius: 30,
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.transparent,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              widget.name,
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 6),
+                            Text(
+                              widget.messageText,
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.grey.shade600,
+                                fontWeight: widget.isMessageRead
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Text(
+                      widget.time,
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: widget.isMessageRead
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
