9c8ec1a7f1bca6f03234fd41c80f70890a3e4317
Milan Tarami <milantarami.dev@gmail.com>
2021-10-06 08:24:28 +0545

    chat detail page

--- lib/screens/chat_detail_page.dart
+++ lib/screens/chat_detail_page.dart
@@ -1,3 +1,4 @@
+import '../models/chat_message_model.dart';
 import 'package:flutter/material.dart';
 
 class ChatDetailPage extends StatefulWidget {
@@ -8,6 +9,29 @@ class ChatDetailPage extends StatefulWidget {
 }
 
 class _ChatDetailPageState extends State<ChatDetailPage> {
+  List<ChatMessage> messages = [
+    ChatMessage(
+      messageContent: "Hello, Will",
+      messageType: "receiver",
+    ),
+    ChatMessage(
+      messageContent: "How have you been?",
+      messageType: "receiver",
+    ),
+    ChatMessage(
+      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+      messageType: "sender",
+    ),
+    ChatMessage(
+      messageContent: "ehhhh, doing OK.",
+      messageType: "receiver",
+    ),
+    ChatMessage(
+      messageContent: "Is there any thing wrong?",
+      messageType: "sender",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +91,102 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
           ),
         ),
       ),
-      body: Container(),
+      body: Stack(
+        children: <Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 10,
+            ),
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Align(
+                  alignment: messages[index].messageType == 'receiver'
+                      ? Alignment.topLeft
+                      : Alignment.topRight,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.blue[200]),
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: const EdgeInsets.only(
+                left: 10,
+                top: 10,
+                right: 10,
+                bottom: 10,
+              ),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Write Message ..',
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
